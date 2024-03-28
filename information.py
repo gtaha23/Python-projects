fe313a38e074838a4f4a9457475ba83c6b42311a
gtaha230812 <159329966+gtaha23@users.noreply.github.com>
2024-03-28 15:48:28 +0300

    Python projects

---
+++ information.py
@@ -0,0 +1,12 @@
+import random
+import time
+
+isim = input("İsminizi giriniz:")
+
+soyisim = input("Soyadınızı giriniz:")
+
+sifre = int(input("Şifrenizi giriniz:"))
+
+email = input("E-Postanızı giriniz:")
+
+print("Bilgileriniz","İsminiz:",isim,"Soyadınız:",soyisim,"Şifreniz:",sifre,"E-posta'nız:",email, sep = "\n")
