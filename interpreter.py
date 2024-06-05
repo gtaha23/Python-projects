67bcb76949ebe46756ed27e42e1cafb67a79f255
gtaha230812 <159329966+gtaha23@users.noreply.github.com>
2024-06-05 22:38:30 +0300

    Python projects

---
+++ interpreter.py
@@ -0,0 +1,9 @@
+print("-----------------------------------")
+print("Welcome to Python CLİ")
+print("You can run python commands here!")
+print("Enjoy!")
+print("-----------------------------------")
+
+
+while True:
+    exec(input('>>> '))
